09b9e3a3f5e3398eea7e6f8021b892be17b960da
Sabra Bildoeau <sabrabilodeau@gmail.com>
2020-06-16 21:49:38 -0700

    Cleanup / commenting

--- place.py
+++ place.py
@@ -1,18 +1,20 @@
 class Place:
+    # constructor; parses the Google place type to create our own
     def _init_(self, place, os):
         api_url = "https://www.google.com/maps/search/?api="
         apikey = 'AIzaSyBMZ6xYlzyjMEZTWiXWF7F6KlvOI-rvWm0'
 
         self.name = place.name
-        self.os = os
+        self.os = os                                        # whether or not this is an original search place
         self.id = place.place_id
-        self.loc = place.geometry.location.toUrlValue()
-        self.has_pt = false
-        self.has_live = false
-        self.live = 0
-        self.covidprec = [false, false, false]          # [MASKS: BOOL, LIMITED_ENTRY: BOOL, EARLY_CLOSE: BOOL]
-        self.poptimes = []
-        self.rank = 0
+        self.loc = place.geometry.location.toUrlValue()     # worried this is for javascript lol; LOOK INTO
+        self.has_pt = false                                 # whether or not it has popular times data; to be determined from DB
+        self.has_live = false                               # whether or not it has live times data; to be determined from DB
+        self.live = 0                                       # live value
+        self.covidprec = [false, false, false]              # [MASKS: BOOL, LIMITED_ENTRY: BOOL, EARLY_CLOSE: BOOL]
+        self.poptimes = []                                  
+        self.rank = 0                                       # to be added to as place parameters are determined
+        self.db = Databse()
         self.url = ('s'
                     '?api=%s'
                     '&query=%s'
@@ -25,48 +27,41 @@ class Place:
         else:
             if self.has_pt:
                 get_pop_times()
-
-        calculate_rank()
         return
 
+    # calls the Databse to get the covid precautions for stores of that name,
+    # as well as if they have popular times data available. will return
+    # true or false depending on if the place was found
     def get_database_info():
-        db = Databse()
-        results = db.search(name)
-        if results[0]:
-            self.covidprec[0] = results[1]
-            self.covidprec[1] = results[2]
-            self.covidprec[2] = results[3]
+        results = self.db.search(name)
+        if results[0]:                                      # found - bool
+            self.covidprec[0] = results[1]                  # masks - bool
+            if self.covidprec[0]:
+                self.rank += 1
+            self.covidprec[1] = results[2]                  # limited_entry - bool
+            if self.covidprec[1]:
+                self.rank += 1
+            self.covidprec[2] = results[3]                  # early_close - bool
+            if self.covidprec[2]:
+                self.rank += 1
             self.has_pt = results[4]
             self.has_live = results[5]
             return true
         return false
 
+    # calls the WebScraper to search for the desired place to scrub its
+    # popular times & live times, if available (updates values of has_pt
+    # & has_live if they are)
     def get_pop_times():
         ws = WebScraper()
-        results = ws.search(self.url)       # [%: INT, LIVE: BOOL]
-        for r in range(results):
-            self.poptimes[r] = r[0]
-            if r[1]:
-                self.has_live = true
-                self.live = r[0] * 0.01
-        return
-
-    def get_live_rank():
-        for t in range(self.poptimes):
-            if t[1]:
-                r = t[0] * 0.01      # if rank live is already sent as a decimal, * 0.01 could be removed
-                self.rank -= r            # if it has a live, we added 1. Now we'll subtract the % of people from that 1.
-        return
-
-    def calculate_rank():
-        if self.covidprec[0]:
+        results = ws.search(self.url)                       # [%: INT, LIVE: BOOL]
+        if results[0][0] != -1:
+            self.has_pt = true
             self.rank += 1
-        if self.covidprec[1]:
-            self.rank += 1
-        if self.covidprec[2]:
-            self.rank += 1
-        if self.has_pt:
-            self.rank += 1
-            if self.has_live:
-                self.rank += (1-self.live)
+            for r in range(results):
+                self.poptimes[r] = r[0]
+                if r[1]:
+                    self.has_live = true
+                    self.live = r[0] * 0.01
+                    self.rank += (1-self.live)                  #
         return
--- recommendor.py
+++ recommendor.py
@@ -1,3 +1,7 @@
+# Recommendor package:
+# Takes in a user's search and ranks each location by their busyness
+# & COVID-19 precautions. Produces an additional search for stores of
+# the same type, tiered in the same manner, for more shopping options
 import json
 import urllib.request as url_req
 import time
@@ -8,9 +12,11 @@ nsr = []        # nearby search results
 rec = []        # recommendations
 loc = ""        # user (estimated) location
 type = ""       # users search type
+orig_name = ""
 
 def Recommendor(search):
     usr = search
+    orig_name = usr[0].name
     loc = usr[0].geometry.location.toUrlValue()
     type = usr[0].types[0]
     get_place_info(usr, true)
@@ -21,15 +27,33 @@ def Recommendor(search):
     final_rec = sort_rec()
     return final_rec
 
+# restricting results to only 5 places, and if we're
+# creating place objects from our nearby search, ensure
+# it isn't a place of the same name
+#
+# places: [Google's 'Place' type]
+# os: original search - boolean
 def get_place_info(places, os):
     i = 0
     for p in range(places):
-        place = Place(p, os)
-        rec += place
         if i == 5:
             return
-        i += 1
+        if !os:
+            if place.name != orig_name:
+                place = Place(p, os)
+                rec += place
+                i += 1
+        else:
+            place = Place(p, os)
+            rec += place
+            i += 1
 
+# sort our recommended list, separating it into two arrays
+# one for the original search results, and one for the recommended
+# sort each of those arrays by the rank of each place
+# return an array containing those 2 arrays, which will be our
+# final recommendation. The first item in each array will be
+# display to the user in text above the graph
 def sort_rec():
     osresults = []                                                  # original search results
     nsresults = []                                                  # nearby search results
@@ -44,12 +68,15 @@ def sort_rec():
     sorted(nsresults, key=lambda place: place.rank)
     return [osresults, nsresults]
 
+# make a call to the Google's Places API to perform a nearby search
 def request_api(url):
     response = url_req.urlopen(url)
     json_raw = response.read()
     json_data = json.loads(json_raw)
     return json_data
 
+# request an array of places near the user that are of the same 'Google type'
+# to be used in our recommendations
 def search_nearby():
     apikey = APIKey
     radius = 5000       # in meters, ~3.1 miles
