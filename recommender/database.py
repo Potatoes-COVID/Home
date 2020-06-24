4c69b86677f67e9336f45b3dd6e1f695820f3db2
Sabra Bildoeau <sabrabilodeau@gmail.com>
2020-06-23 18:56:57 -0700

    Changes after implementating database.search()
    Updated code to make it actually run
    ** Did so by referencing nearbysearch.py which has been tested
    
    TODO:
    database.py --
     add_entry method
     update_entry_id method
    
    nearbysearch.py --
      MOSTLY TEST

--- recommender/database.py
+++ recommender/database.py
@@ -2,28 +2,23 @@ import pandas as pd
 import numpy as np
 
 class Database(object):
-    def __init__(self, plus_code):
+    def __init__(self, place):
         self._data = pd.read_csv('data/places.csv', index_col=0)
-        print(self._data['plus_code'][0])
-        if plus_code == self._data['plus_code'][0]:
-            print('suuuucka')
-        return
+        # print(self._data.loc[lat_lng])
+        self.search(place)
 
+    def search(self, place):
+        print('Performing Database search ...')
 
-
-#    print(get_data())
-
-# for (index, row) in places.iterrows():
-#    print("Populating " + str(index))
-#    moments = get_place_popular_moments(row.place_id)
-#    if moments != None:
-#        self.data.at[index, 'monday'] = moments[0]['data']
-#        self.data.at[index, 'tuesday'] = moments[1]['data']
-#        self.data.at[index, 'wednesday'] = moments[2]['data']
-#        self.data.at[index, 'thursday'] = moments[3]['data']
-#        self.data.at[index, 'friday'] = moments[4]['data']
-#        self.data.at[index, 'saturday'] = moments[5]['data']
-#        self.data.at[index, 'sunday'] = moments[6]['data']
-
-#places.to_csv('data/places_with_moments.csv')
-#print(self.data)
+        if place.plus_code in self._data.values:
+            place.in_db = True
+            place.subtype = self._data.loc[lat_lng].iloc[1]
+            place.covidprec[0] = self._data.loc[lat_lng].iloc[2]
+            place.covidprec[1] = self._data.loc[lat_lng].iloc[3]
+            place.covidprec[2] = self._data.loc[lat_lng].iloc[4]
+            place.covidprec[3] = self._data.loc[lat_lng].iloc[5]
+            place.covidprec[4]= self._data.loc[lat_lng].iloc[6]
+            place.covidprec[5] = self._data.loc[lat_lng].iloc[7]
+            place.has_pt = self._data.loc[lat_lng].iloc[8]
+            place.has_live = self._data.loc[lat_lng].iloc[9]
+            print('Woo hoo ! Found !')
--- recommender/usersearch.py
+++ recommender/usersearch.py
@@ -1,13 +1,13 @@
-import place
+import place as p
 
-class UserSearch:
-    def _init_(self, results):
+class UserSearch(object):
+    def __init__(self, results):
             self.name = results[0]['name']
             self.type = results[0]['types'][0]
             self.places = []
             self.recs = []
-            create_places(results)
-            print("Name : %s", self.name)
+            self.create_places(results)
+            print("Name : ", self.name)
             return
 
     # for each place in places
@@ -16,30 +16,30 @@ class UserSearch:
     # else, create a place object, check the database for it
     # if it has a live value, add it to our recs array, else add it to our places
     def create_places(self, places):
-        for p in range(places):
-            num_recs = self.recs.length()
-            num_places = self.places.length()
+        for pl in range(len(places)):
+            num_recs = len(self.recs)
+            num_places = len(self.places)
             if num_recs == 5: break
-            else if num_places == 5 && num_recs > 1: break
+            if num_places == 5:
+                if num_recs > 1: break
             else:
-                place = Place(p)
+                place = p.Place(places[pl])
                 if place.has_live:
-                    self.recs += place
+                    self.recs.append(place)
                 else:
-                    self.places += place
-        sort_recs()
+                    self.places.append(place)
+        self.sort_recs()
 
     # if num_recs < 5 [we have < 5 places with live times], add places from
     # our places array to our recs. sort the places by their rank, first !
     # then sort the recs array
     def sort_recs(self):
-        num_recs = self.recs.length()
+        num_recs = len(self.recs)
         if num_recs < 5:
             sorted(self.places, key=lambda place: place.rank)
             max = 4
             for index in range(max):
-                self.recs += self.places[index]
+                self.recs.append(self.places[index])
                 num_recs += 1
-                if num_recs == 5:
-                    break
+                if num_recs == 5: break
         sorted(self.recs, key=lambda place: place.rank)
